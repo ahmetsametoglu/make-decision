cb4e6a95d643c86a2670e584a378550d1cf1939c
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-09 18:01:34 +0200

    c-feat: create app store

---
+++ packages/client/src/api/redux/app/action.ts
@@ -0,0 +1,15 @@
+import { ActionsUnion } from './../../helper/type.helper';
+import { INotification, AppActionType, NotificationType } from './type';
+import { action } from 'typesafe-actions';
+
+export const AppActions = {
+  showNotification: (type: NotificationType, message: string, title?: string, duration?: number) =>
+    action(AppActionType.SHOW_NOTIFICATION, { type, message, title, duration }),
+
+  showNotificationSuccess: (notification: INotification) =>
+    action(AppActionType.SHOW_NOTIFICATION_SUCCESS, { notification }),
+
+  deleteNotification: (notificationId: string) => action(AppActionType.DELETE_NOTIFICATION, { notificationId }),
+};
+
+export type AppAction = ActionsUnion<typeof AppActions>;
